67e423483adb6ca1b37095fb1f6c2c49b0944107
Koonail <330739264@qq.com>
2018-04-13 17:59:41 +0800

    丰富代码

--- src/com/koonail/structures/Linked/LinkedTest.java
+++ src/com/koonail/structures/Linked/LinkedTest.java
@@ -4,10 +4,41 @@ public class LinkedTest {
     public static void main(String[] args) {
         MyLinked myLinked = new MyLinked();
         myLinked.addNode(1);
+        myLinked.addNode(3);
         myLinked.addNode(2);
+        myLinked.addNode(5);
+        myLinked.addNode(4);
+        myLinked.printLinked();
+        // 获得链表的长度
+        System.out.println("链表长度为：" + myLinked.length());
         // 获得头节点的值
-        System.out.println(myLinked.getHead().data);
+        System.out.println("链表头节点的值为：" + myLinked.getHead().data);
         // 获得指定节点的值
-        System.out.println(myLinked.getNode(2).data);
+        int index = 2;
+        System.out.println("链表第" + index + "个节点的值为" + myLinked.getNode(index).data);
+        // 修改指定节点的值
+        // myLinked.setNode(index,4);
+        //System.out.println("链表第" + index + "个节点的值为" + myLinked.getNode(index).data);
+        // 删除指定节点
+        //myLinked.deleteNode(3);
+
+
+        // 常用链表操作
+        // 链表反转
+        //myLinked.reverseLinked();
+        // 获得链表的中间节点
+        System.out.println("链表的中间节点为：" + myLinked.getMidNode().data);
+        // 获得链表的倒数第二个节点
+        System.out.println("链表的倒数第二个节点为：" + myLinked.getReciprocalNode(2).data);
+        // 链表排序
+        myLinked.sort();
+        // 删除链表中的重复元素
+        myLinked.uniq();
+        // 倒序打印当前链表
+        System.out.print("当前链表的倒序输出为：");
+        myLinked.reversePrint(myLinked.getHead());
+        System.out.println();
+        // 打印当前链表
+        myLinked.printLinked();
     }
 }
--- src/com/koonail/structures/Linked/MyLinked.java
+++ src/com/koonail/structures/Linked/MyLinked.java
@@ -1,8 +1,10 @@
 package com.koonail.structures.Linked;
 
+import java.util.List;
+
 /**
  * @Description:自定义链表实现
- * @author WEN KANG
+ * @author: WEN KANG
  * @time 2018年4月13日 星期五
  */
 public class MyLinked {
@@ -40,16 +42,19 @@ public class MyLinked {
     }
 
     /**
-     * 获取指定下标的节点的值
+     * 获取指定节点的值
      * @param index
      * @return
      */
     public Node getNode(int index){
-        // 如果头节点为空，则链表为空，返回空值
-        if (head == null){
-            return null;
+        // 越界判断
+        if (index > length()){
+            try {
+                throw new Exception("越界！！！");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        // 头节点不为空
         int curIndex = 1;
         Node curNode = head;
         while(curNode != null){
@@ -63,12 +68,226 @@ public class MyLinked {
     }
 
     /**
-     * 修改指定下标的节点值
+     * 修改指定节点值
+     * @param index
+     * @param newData 新的值
+     * @return
+     */
+    public  boolean setNode(int index, int newData){
+        // 如果给出的下标超出链表长度，则抛出越界异常
+        if (index > length()){
+            try {
+                throw new Exception("越界！！！");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        int curIndex = 1;
+        Node curNode = head;
+        while (curNode != null){
+            if (index == curIndex){
+                curNode.data = newData;
+                return true;
+            }
+            curIndex ++;
+            curNode = curNode.next;
+        }
+        return false;
+    }
+
+    /**
+     * 删除指定节点
      * @param index
      * @return
      */
-    public  boolean setNode(int index){
+    public boolean deleteNode(int index){
+        // 越界判断
+        if (index > length()){
+            try {
+                throw new Exception("越界！！！");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        int cruIndex = 1;
+        Node preNode = null;
+        Node curNode = head;
+        Node nextNode = curNode.next;
+        while (curNode != null){
+            if (index == cruIndex){
+                // 只剩一个节点
+                if (nextNode == null) {
+                    curNode = null;
+                    return true;
+                } else if (index == 1){
+                    // 如果需要删除头节点
+                    head = head.next;
+                    return true;
+                }
+                preNode.next = curNode.next;
+                return true;
+            }
+            preNode = curNode;
+            curNode = curNode.next;
+            cruIndex ++;
+        }
         return false;
+    }
+
+    /**
+     * 链表反转
+     */
+    public void reverseLinked()
+    {
+        Node preNode = null;
+        Node curNode = head;
+        while (curNode != null) {
+            Node nextNode = curNode.next;
+            // 当前节点为最后一节点时，反转完毕
+            if (nextNode == null){
+                head = curNode;
+            }
+            // 当前节点指向原上一节点
+            curNode.next = preNode;
+            // 对于下一节点来说，当前节点就是原上一节点
+            preNode = curNode;
+            // 当前节点往后移动
+            curNode = nextNode;
+        }
+    }
+
+    /**
+     * 获取链表的中间节点
+     * 采用快慢指针的方式，快指针一次走两步，慢指针一次走一步，当快指针走到末尾时，满指针刚好走到中间
+     * @return
+     */
+    public Node getMidNode()
+    {
+        Node fastPointer = head;
+        Node slowPointer = head;
+        // 至少需要三个节点才能体现出中间节点
+        while (fastPointer != null && fastPointer.next != null && fastPointer.next.next != null){
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+        }
+        return slowPointer;
+    }
+
+    /**
+     * 获取链表的倒数第index个节点
+     * 还是采用双指针的方式，让指针q先移动index，然后再让p和q一起移动，当q移动到末尾时，p则是倒数第index个节点
+     * @param index
+     * @return
+     */
+    public Node getReciprocalNode(int index)
+    {
+        // 判断越界
+        if (index > length()){
+            try {
+                throw new Exception("越界！！！");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        Node q = head;
+        Node p = head;
+        // 让q先移动index
+        for (int i = 0;i < index;i++){
+            q = q.next;
+        }
+        while (q != null){
+            q = q.next;
+            p = p.next;
+        }
+        return p;
+    }
+
+    /**
+     * 链表排序
+     * 采用冒泡排序，不用改变节点位置，交换节点的值就行
+     */
+    public void sort(){
+        Node curNode = head;
+        int tempData = 0;
+        while (curNode.next != null){
+            Node nextNode = curNode.next;
+            while (nextNode != null){
+                if (curNode.data > nextNode.data){
+                    tempData = curNode.data;
+                    curNode.data = nextNode.data;
+                    nextNode.data = tempData;
+                }
+                nextNode = nextNode.next;
+            }
+            curNode = curNode.next;
+        }
+    }
+
+    /**
+     * 去除链表中的重复节点
+     */
+    public void uniq()
+    {
+        Node curNode = head;
+        while (curNode != null){
+            Node temp = curNode;
+            while (temp.next != null){
+                Node nextNode = temp.next;
+                if (curNode.data == nextNode.data){
+                    temp.next = temp.next.next;
+                }
+                temp = temp.next;
+            }
+            curNode = curNode.next;
+        }
+    }
+
+    /**
+     * 递归倒序输出链表
+     */
+    public void reversePrint(Node head){
+        if (head != null){
+            reversePrint(head.next);
+            System.out.print(head.data + " ");
+        }
+    }
+
+    /**
+     * 获取链表的长度
+     * @return
+     */
+    public int length(){
+        // 链表为空，返回0
+        if (head == null){
+            return 0;
+        }
+        // 加上头节点
+        int length = 1;
+        Node curNode = head;
+        while (curNode.next != null) {
+            curNode = curNode.next;
+            length ++;
+        }
+        return length;
+    }
+
+    /**
+     * 以数组的形式打印链表所有节点的值
+     * @return
+     */
+    public void printLinked()
+    {
+        if (head == null){
+            return;
+        }
+        Node curNode = head;
+        System.out.print("当前链表为：");
+        while(curNode != null)
+        {
+            System.out.print(curNode.data + " ");
+            curNode = curNode.next;
+        }
+        System.out.println();
     }
 
     public Node getHead() {
