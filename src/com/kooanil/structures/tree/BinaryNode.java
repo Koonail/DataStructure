c8721622a911b891f227bfef71200f1dfbb62196
Koonail <330739264@qq.com>
2018-05-08 22:54:06 +0800

    二叉查找树

---
+++ src/com/kooanil/structures/tree/BinaryNode.java
@@ -0,0 +1,22 @@
+package com.kooanil.structures.tree;
+
+/**
+ * 节点类
+ */
+public class BinaryNode {
+    /** 节点值*/
+    int data;
+    /** 左子节点*/
+    BinaryNode left;
+    /** 右子节点*/
+    BinaryNode right;
+    public BinaryNode(int data){
+        this(data,null,null);
+    }
+
+    public BinaryNode(int data,BinaryNode lt,BinaryNode rt){
+        data = data;
+        left = lt;
+        right = rt;
+    }
+}
---
+++ src/com/kooanil/structures/tree/BinarySearchTree.java
@@ -0,0 +1,8 @@
+package com.kooanil.structures.tree;
+
+/**
+ * 二叉查找树
+ */
+public class BinarySearchTree {
+    
+}
